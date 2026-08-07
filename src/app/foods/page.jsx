18abe0e81cd51acf44caf151795c8a20f81b1bab
flynn-hagidok <flynnhagidok@gmail.com>
2026-08-07 23:20:39 +0600

    added food card

--- src/app/foods/page.jsx
+++ src/app/foods/page.jsx
@@ -1,3 +1,5 @@
+import FoodCard from "@/components/Cards/FoodCard";
+
 const getFoods = async () => {
     const res = await fetch(' https://taxi-kitchen-api.vercel.app/api/v1/foods/random');
     const data = await res.json();
@@ -11,6 +13,12 @@ const Foods = async () => {
     return (
         <div className="">
             <h2>Total {foods.length} Foods Found</h2>
+
+            <div className="grid grid-cols-3 gap-6 my-10">
+                {
+                    foods.map(food => <FoodCard key={food.id} food={food}></FoodCard>)
+                }
+            </div>
         </div>
     )
 };
---
+++ src/components/Cards/FoodCard.jsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const FoodCard = ({ food }) => {
+    const { id, title, category, foodImg, price } = food;
+
+    return (
+        <div className="overflow-hidden rounded-xl border bg-white shadow-sm transition duration-300 hover:shadow-lg">
+            <div className="relative h-56 w-full">
+                <Image
+                    src={foodImg}
+                    alt={title}
+                    fill
+                    className="object-cover p-4 rounded-2xl"
+                />
+            </div>
+
+            <div className="space-y-3 p-5">
+                <div>
+                    <span className="rounded-full bg-orange-100 px-3 py-1 text-xs font-medium text-orange-600">
+                        {category}
+                    </span>
+                </div>
+
+                <h2 className="line-clamp-2 text-black text-xl font-bold">
+                    {title}
+                </h2>
+
+                <p className="text-2xl font-bold text-orange-500">
+                    ${price}
+                </p>
+
+                <div className="flex gap-3">
+                    <button className="flex-1 rounded-lg bg-orange-500 px-4 py-2 font-medium text-white transition hover:bg-orange-600">
+                        Add to Cart
+                    </button>
+
+                    <Link
+                        href={`/foods/${id}`}
+                        className="flex-1 rounded-lg border border-orange-500 px-4 py-2 text-center font-medium text-orange-500 transition hover:bg-orange-500 hover:text-white"
+                    >
+                        View Details
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FoodCard;
